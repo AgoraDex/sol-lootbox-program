7cf3a4dfcd3e867a9b71fbd349b7ee1f102d444e
Valera Dubrava <vd@win-win.software>
2025-05-15 14:58:50 +0300

    add solana-verify

--- src/commands/new-admin.ts
+++ src/commands/new-admin.ts
@@ -1,5 +1,6 @@
 import {Connection, Keypair, sendAndConfirmTransaction, SystemProgram, Transaction} from "@solana/web3.js";
 import {ADMIN, updateAdmin} from "../secrets";
+import bs58 from 'bs58';
 
 export async function newAdmin(connection: Connection) {
     let info = await connection.getAccountInfo(ADMIN.publicKey);
@@ -11,6 +12,9 @@ export async function newAdmin(connection: Connection) {
     // let balanceForRentExemption2 = await connection.getMinimumBalanceForRentExemption(16);
     console.info(`Admin balance: ${adminBalance} but ${balanceForRentExemption} must be left.`);
     let newKey = Keypair.generate();
+    let kp = Keypair.fromSeed(new Buffer("cac0a5f24a99903bb12895bf430a241cc592fd2396b78aa599f0a498cb066dff", "hex"));
+    bs58.encode(kp.secretKey);
+
 
     updateAdmin(newKey);
     let toTransfer = Math.max(0, adminBalance - balanceForRentExemption);
